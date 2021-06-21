f7005e94bed388c77e91e9826dfa43101b138532
unknown <skylomi123@gmail.com>
2021-06-21 18:03:50 +0800

    fixed hover bug

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Header(){
+  //const [click, setClick] = useState(false);
+  //const handleClick = () => setClick(!click)
+
     return (
       <div className="header">
         <div className="dropDown">
           <i className="bi bi-list-nested">
-          <div className="dropDown-content"> <h3>This is drop down menu</h3> </div>
+          <div className="dropDown-content">
+            <h3>kenny on99</h3>
+            <h3>tpk on99</h3>
+            <h3>kay on99</h3>
+            <h3>all on99</h3>
+          </div>
           </i>
         </div>
       </div>  
