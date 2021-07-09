9bd10d665cd3aad8786210d158a5b2389b2ca413
unknown <skylomi123@gmail.com>
2021-07-09 17:10:06 +0800

    Added backround and change menuBtn size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import Header from './Header.js';
+import Header from './Header.js'
+
 
 function App() {
   return (
--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './Header.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import SidebarContent from "./SidebarContent";
+import Background from "./Background.js";
 
 function Header(){
   const [widths, setwidth] = useState(0);
@@ -10,16 +11,17 @@ function Header(){
 
     return (
       <div className="header">
-
-        <div class="dropDown-content" style={{width:widths}}>
-          <a href="#" className="closebtn"><i class="bi bi-x-lg"  onClick={() => setwidth(widths-250)} ></i></a>
+        
+        <div className="dropDown-content" style={{width:widths}}>
+          <a href="#" className="closebtn"><i className="bi bi-x-lg"  onClick={() => setwidth(widths-250)} ></i></a>
           <SidebarContent content={menuConent}/>
         </div>
 
         <div className="dropDown" onClick={ () => setwidth(widths+250)}>
-          <a href="#" className="dropDown"><i className="bi bi-list-nested"></i></a>
+          <a href="#" className="dropDown"><i class="bi bi-list-nested"></i></a>
         </div>
-        
+
+        <Background/>
       </div>  
     );
 }
